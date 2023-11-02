18c115df25b16bd9fdf9a22c3519e08fd5c96fdc
WhoisBsa <mbscbsjbs@gmail.com>
2023-11-02 19:21:17 -0300

    ft: add taiga ui module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TUI_SANITIZER, TuiAlertModule, TuiButtonModule, TuiDialogModule, TuiExpandModule, TuiRootModule } from "@taiga-ui/core";
+import { TUI_LANGUAGE, TUI_PORTUGUESE_LANGUAGE } from '@taiga-ui/i18n';
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
 
+
+import { of } from "rxjs";
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      BrowserAnimationsModule,
+      TuiRootModule,
+      TuiDialogModule,
+      TuiAlertModule,
+      TuiExpandModule,
+      TuiButtonModule,
+],
+  providers: [
+    {provide: TUI_SANITIZER, useClass: NgDompurifySanitizer},
+    { provide: TUI_LANGUAGE, useValue: of(TUI_PORTUGUESE_LANGUAGE)}
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
